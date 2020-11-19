88dafa09b5064e85541104fa4dfbad0721b60142
Zyycyx <pth@gmail.hu>
2020-11-19 22:40:34 +0100

    Updated for publishing

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
+import pathlib
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
 
 setuptools.setup(
-    name="wtfpy-memorymanagement-Zyycyx", 
-    version="0.0.1",
-    author="Zyycyx",
-    author_email="pth@gmail.hu",
-    description="A basic module to work with memory addresses and references.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Zyycyx/wtfpy",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    	name="wtfpy-memoman",
+    	version="1.0.0",
+    	author="Zyycyx",
+    	author_email="pth@gmail.hu",
+    	description="A basic module to work with memory addresses and references.",
+    	long_description=README,
+    	long_description_content_type="text/markdown",
+    	url="https://github.com/Zyycyx/wtfpy",
+    	packages=setuptools.find_packages(),
+    	classifiers=[
+        	"Programming Language :: Python :: 3.8",
+        	"License :: OSI Approved :: MIT License",
+        	"Operating System :: OS Independent",
+    	],
+	include_package_data=True,
+	install_requires=["sys", "array"],
+    	python_requires='>=3.7',
 )
 
