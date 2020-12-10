7873fa3d6641a781ea3e034bd861e3c064ef94cf
Zyycyx <pth@gmail.hu>
2020-12-10 10:58:16 +0100

    Refresh 202012

--- WtfPy2/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from distutils.core import setup, Extension
-
-setup(
-	name = "WtfPy2",
-	version = "2.0",
-	ext_modules = [Extension("WtfPy2", ["bind.c", "libmypy.c"])]
-	)
--- WtfPy2/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import WtfPy2
-
-x = 5
-
-print(WtfPy2.hello())
-print(WtfPy2.getVal(x, "X"))
-help(WtfPy2)
